50529052bc356d7a825f01ffa0dd548bdc1c0446
SandstormNorthxyz <collingkeegan@gmail.com>
2026-06-19 15:14:07 +0100

    pindefs

--- main.ts
+++ main.ts
@@ -2,11 +2,33 @@
 //% groups='["Bridge","Sensors"]'
 namespace towerBridge {
 
+    //pindefs.h memorial let statements
+    let PWR_IMON_PIN = AnalogPin.P0
+    let NORTH_IMON_PIN = AnalogPin.P1
+    let SOUTH_IMON_PIN = AnalogPin.P2
+    let SENSE2_PIN = AnalogPin.P3
+    let SENSE1_PIN = AnalogPin.P4
+    let N_BASC_LED_PIN = DigitalPin.P6
+    let N_TOWER_LED_PIN = DigitalPin.P7
+    let SIN2 = DigitalPin.P8
+    let SIN1 = DigitalPin.P9
+    let LOWTRIP_PIN = DigitalPin.P10
+    let S_BASC_LED_PIN = DigitalPin.P12
+    let S_TOWER_LED_PIN = DigitalPin.P13
+    let NIN2 = DigitalPin.P14
+    let LED_TX_PIN = DigitalPin.P15 //CTX/COPI pin of the SPI peripheral
+    let NIN1 = DigitalPin.P16
+    //I2C pins left unassigned because they are configured by the I2C peripheral
+
     let muxI2CAddr: uint8 = 0x70
     let encoderI2CAddr: uint8 = 0x36
     let colorI2CAddr: uint8 = 0x44 //0x52
 
-    function setEncoderChannel(channel: number) { //this also implicitly
+    function swapBytes(value: NumberFormat.UInt16LE) {
+        return ((value & 0xFF00) >> 8) | ((value & 0x00FF) << 8)
+    }
+
+    function setEncoderChannel(channel: number) { //this also implicitly enables the color sensor channel, since there's no address conflict so that should always be enabled
         pins.i2cWriteNumber(muxI2CAddr, channel == 0 ? 0b0101 : 0b0110, NumberFormat.UInt8LE, false)
     }
 
@@ -17,13 +39,22 @@ namespace towerBridge {
         return rawAngle
     }
 
-    function swapBytes(value: NumberFormat.UInt16LE) {
-        return ((value & 0xFF00) >> 8) | ((value & 0x00FF) << 8)
+    //returns the current drawn from the polled motor in milliamps
+    function pollMotorVoltage(pin: number) {
+        return pins.analogReadPin(pin) * (3.3 / 1024.0) * (700.0 / 2.5) //10bit ADC, 700mA/2.5V VREF
     }
 
     basic.forever(function () {
         //code in here will run within the fibre scheduler, scheduled cooperatively, with a 6ms polling time
-        basic.pause(1500)
+        /*
+        TODO:
+         poll encoders
+         poll motor current sense
+         turn off motors if encoders over limit in either direction
+         set LEDs if encoders over limit in either direction
+         maybe write hiccup timer code for if lowtrip isn't enabled and current is over limit (not sure exactly what the best logic for this would be, will probably write this once we have final model assembled)
+         */
+        basic.pause(24) //run monitoring loop at ~42Hz (every 4 scheduler cycles when not interrupted)
     })
 
     /**
