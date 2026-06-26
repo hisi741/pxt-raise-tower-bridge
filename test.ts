040d2986bdbb143ef5f7381d21c1b42d38f19a4f
SandstormNorthxyz <collingkeegan@gmail.com>
2026-06-26 15:24:42 +0100

    test file update

--- test.ts
+++ test.ts
@@ -1,15 +1,14 @@
 basic.forever(function () {
     if (bridgeSensors.isShipPresent()) {
-        bridgeLighting.setTowerLightingBrightness(BridgeSide.Left, 100)
-        if (bridgeSensors.getColorSensorReading() == bridgeSensors.getColor(ColorSensorColor.Red)) {
+        if (bridgeSensors.checkColor(ColorSensorColor.Red)) {
             bridgeLighting.setTowerLightingColorHex(BridgeSide.Left, 0xff0000)
         } else {
             bridgeLighting.setTowerLightingColorHex(BridgeSide.Left, 0x007fff)
         }
-        if (bridgeSensors.getShipTransitDirection() == bridgeSensors.upriver()) {
-            bridgeLighting.setTowerLightingColorHex(BridgeSide.Right, 0x00ff00)
-        } else {
+        if (bridgeSensors.checkShipSailingDirection(ShipSailingDirection.Upriver)) {
             bridgeLighting.setTowerLightingColorHex(BridgeSide.Right, 0xff8000)
+        } else {
+            bridgeLighting.setTowerLightingColorHex(BridgeSide.Right, 0x00ff00)
         }
         basculeMotors.raiseLowerBascule(BridgeSide.Left, BasculeDirection.Raise)
         basculeMotors.raiseLowerBascule(BridgeSide.Right, BasculeDirection.Raise)
@@ -19,8 +18,6 @@ basic.forever(function () {
         basculeMotors.raiseLowerBascule(BridgeSide.Left, BasculeDirection.Lower)
         basculeMotors.raiseLowerBascule(BridgeSide.Right, BasculeDirection.Lower)
     } else {
-        bridgeLighting.setTowerLightingBrightness(BridgeSide.Left, 0)
+        basic.pause(100)
     }
-    basic.pause(100)
 })
-
